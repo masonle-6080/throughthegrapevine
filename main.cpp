a1da1fa8b55621ac9a1669d40237421e5f5cc6e5
Mason Le <masonle65@gmail..com>
2025-10-19 14:17:20 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+
+#include <string>
+#include <unordered_map>
+
+
+
+void getInput(int &n, int &m, int &d){
+    std::cin >> n >> m >> d;
+    
+}
+
+int main() {
+    
+    int n, m, d;
+    std::unordered_map<std
+
+    getInput(n, m, d);
+
+    
+
+
+    // Test printing
+    std::ofstream outFile("output.txt");
+
+    outFile << "first line inputs: "<< n << " " << m << " " << d << "\n";
+    return 0;
+}
